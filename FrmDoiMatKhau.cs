3a825c76179ef075dc1046efa0cf88246386526d
HuyHung15 <hhungtran98@gmail.com>
2018-11-27 15:31:49 +0700

    Đổi mật khẩu tạm ổn

---
+++ FrmDoiMatKhau.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLMayTram
+{
+    public partial class FrmDoiMatKhau : Form
+    {
+        public FrmDoiMatKhau()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        void kiemtranhapMK()
+        {
+            if (txtMatKhauCu.Text != "")
+            {
+                if (txtMatKhauMoi.Text != "")
+                {
+                    if (txtNhapLaiMKM.Text != "")
+                    {
+
+
+                        if (txtMatKhauMoi.Text == txtNhapLaiMKM.Text)
+                        {
+                            MessageBox.Show("Change password successfully! ");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Those password did'nt match. \nTry again. ");
+                        }
+                    }
+                    else MessageBox.Show("Confirm new password is empty");
+                }
+                else MessageBox.Show("The new password is empty");
+            }
+            else MessageBox.Show("The old password is empty");
+            
+            
+        }
+
+        private void btxXacNhan_Click(object sender, EventArgs e)
+        {
+            kiemtranhapMK();
+        }
+    }
+}
---
+++ FrmDoiMatKhau.designer.cs
@@ -0,0 +1,163 @@
+﻿namespace QLMayTram
+{
+    partial class FrmDoiMatKhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDoiMatKhau));
+            this.txtMatKhauCu = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMatKhauMoi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNhapLaiMKM = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btxXacNhan = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMatKhauCu
+            // 
+            this.txtMatKhauCu.Font = new System.Drawing.Font("KV-Helv-cd", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhauCu.Location = new System.Drawing.Point(235, 18);
+            this.txtMatKhauCu.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMatKhauCu.Name = "txtMatKhauCu";
+            this.txtMatKhauCu.PasswordChar = '*';
+            this.txtMatKhauCu.Size = new System.Drawing.Size(244, 33);
+            this.txtMatKhauCu.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("KV-Helv-cd", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 26);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Old Password:";
+            // 
+            // txtMatKhauMoi
+            // 
+            this.txtMatKhauMoi.Font = new System.Drawing.Font("KV-Helv-cd", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhauMoi.Location = new System.Drawing.Point(235, 67);
+            this.txtMatKhauMoi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMatKhauMoi.Name = "txtMatKhauMoi";
+            this.txtMatKhauMoi.PasswordChar = '*';
+            this.txtMatKhauMoi.Size = new System.Drawing.Size(244, 33);
+            this.txtMatKhauMoi.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("KV-Helv-cd", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 26);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "New Password:";
+            // 
+            // txtNhapLaiMKM
+            // 
+            this.txtNhapLaiMKM.Font = new System.Drawing.Font("KV-Helv-cd", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNhapLaiMKM.Location = new System.Drawing.Point(235, 116);
+            this.txtNhapLaiMKM.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNhapLaiMKM.Name = "txtNhapLaiMKM";
+            this.txtNhapLaiMKM.PasswordChar = '*';
+            this.txtNhapLaiMKM.Size = new System.Drawing.Size(244, 33);
+            this.txtNhapLaiMKM.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("KV-Helv-cd", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 26);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Verify New Password:";
+            // 
+            // btxXacNhan
+            // 
+            this.btxXacNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btxXacNhan.Location = new System.Drawing.Point(235, 161);
+            this.btxXacNhan.Margin = new System.Windows.Forms.Padding(4);
+            this.btxXacNhan.Name = "btxXacNhan";
+            this.btxXacNhan.Size = new System.Drawing.Size(139, 33);
+            this.btxXacNhan.TabIndex = 3;
+            this.btxXacNhan.Text = "Confirm";
+            this.btxXacNhan.UseVisualStyleBackColor = true;
+            this.btxXacNhan.Click += new System.EventHandler(this.btxXacNhan_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(382, 161);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(97, 33);
+            this.btnThoat.TabIndex = 4;
+            this.btnThoat.Text = "Exit";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // FrmDoiMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 207);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btxXacNhan);
+            this.Controls.Add(this.txtNhapLaiMKM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMatKhauMoi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMatKhauCu);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmDoiMatKhau";
+            this.Text = "Change Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMatKhauCu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMatKhauMoi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNhapLaiMKM;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btxXacNhan;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
